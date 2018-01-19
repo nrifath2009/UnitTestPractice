ab1d3f011a1a7f1cb3f6bff9f3a1b86b13117829
nrifath2009 <nrifath2009@gmail.com>
2018-01-19 16:25:10 +0600

    Unit Test Practice Started

---
+++ Calculator/SalaryCalculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class SalaryCalculator
+    {
+        const int hoursInYear = 2000;
+        public decimal GetAnnualSalary(int hourlyWage)=> hourlyWage * hoursInYear;
+        public decimal GetHourlyWage(int annualSalary)=> annualSalary / hoursInYear;
+        
+    }
+}
--- UnitTestPractice/UnitTest1.cs
+++ UnitTestPractice/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calculator;
 
 namespace UnitTestPractice
 {
@@ -7,8 +8,19 @@ namespace UnitTestPractice
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void AnnualSalaryTest()
         {
+            SalaryCalculator sc =new SalaryCalculator();
+            decimal annualSalary = sc.GetAnnualSalary(50);
+            Assert.AreEqual(100000,annualSalary);
+        }
+
+        [TestMethod]
+        public void HourlyWageTest()
+        {
+            SalaryCalculator sc = new SalaryCalculator();
+            decimal hourlyWage = sc.GetHourlyWage(52000);
+            Assert.AreEqual(26, hourlyWage);
         }
     }
 }

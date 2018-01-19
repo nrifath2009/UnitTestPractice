0796a2634e387704f51ca7ba3905aa066955652c
nrifath2009 <nrifath2009@gmail.com>
2018-01-19 17:24:31 +0600

    Refactor

---
+++ UnitTestProject2/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Polymorphism;
+
+namespace UnitTestProject2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CalculateWeeklySalaryForEmployeeTest_70wage55hoursReturns2800Dollars()
+        {
+            //Arrange
+            int weeklyHours = 55;
+            int wage = 70;
+            int salary = 1;
+            Employee e = new Employee();
+
+            string expectedResponse = string.Format("This ANGRY EMPLOYEE worked {0} hrs. " +
+                                                    "Paid for 40 hrs at $ {1}" +
+                                                    "/hr = ${2}", weeklyHours, wage, salary);
+
+            //Act
+            //Assert
+        }
+    }
+}
